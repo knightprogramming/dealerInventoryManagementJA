d2b5bf11b94f9a372ecc29721b262f5953f91a9e
Chris Little <Chris@JA.local>
2018-10-04 12:13:53 -0600

    php

---
+++ dealerInventoryManagementJA.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package dealerInventoryManagementJA
+ */
+/*
+Plugin Name: dealerInventoryManagementJA
+Plugin URI: https://github.com/knightprogramming/jakesDealerPlugin
+Description: Plugin to modify Woocommerce metadata for dealer specific pricing
+Version: 1.0.0
+Author: Jacob Knight
+Author URI: knightprogramming.com
+License: GPLv2 or later
+Text Domain: dealerInventoryManagement
+*/
